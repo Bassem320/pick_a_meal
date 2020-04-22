2d5b243b5386223b9a19ab7a10013fe745306be2
Bassem <Bsaleh103@gmail.com>
2020-04-22 03:14:42 +0200

    HomeActivity and HomeViewModel Connected

--- app/src/main/java/com/tofi/pickameal/view/HomeActivity.java
+++ app/src/main/java/com/tofi/pickameal/view/HomeActivity.java
@@ -1,16 +1,53 @@
 package com.tofi.pickameal.view;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.tofi.pickameal.R;
+import com.tofi.pickameal.model.Meal;
+import com.tofi.pickameal.viewmodel.HomeViewModel;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private HomeViewModel mViewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mViewModel = new HomeViewModelFactory(this).create(HomeViewModel.class);
+
+        // Quick Meal Button Handler
+        FloatingActionButton btnQuickMeal = findViewById(R.id.btn_quick_meal);
+        btnQuickMeal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mViewModel.getQuickMeal();
+            }
+        });
+    }
+
+    public void setMeal(Meal meal){
+
+    }
+
+
+    static class HomeViewModelFactory implements ViewModelProvider.Factory {
+        private HomeActivity mActivity;
+
+
+        HomeViewModelFactory(HomeActivity activity) {
+            mActivity = activity;
+        }
+
+
+        @Override
+        public <T extends ViewModel> T create(Class<T> modelClass) {
+            return (T) new HomeViewModel(mActivity);
+        }
     }
 }
--- app/src/main/java/com/tofi/pickameal/viewmodel/HomeViewModel.java
+++ app/src/main/java/com/tofi/pickameal/viewmodel/HomeViewModel.java
@@ -1,4 +1,36 @@
 package com.tofi.pickameal.viewmodel;
 
-public class HomeViewModel {
+import android.util.Log;
+
+import androidx.lifecycle.ViewModel;
+
+import com.tofi.pickameal.model.Meal;
+import com.tofi.pickameal.view.HomeActivity;
+
+import java.util.Calendar;
+
+public class HomeViewModel extends ViewModel {
+
+    private  HomeActivity mHomeActivity;
+
+    public HomeViewModel(HomeActivity homeActivity){
+            mHomeActivity = homeActivity;
+    }
+
+    public void getQuickMeal() {
+        // Still need to be handled
+        Meal quickMeal = new Meal();
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+
+        if(hour>0 && hour<5){
+            Log.i("PickAMeal","Sohor Time");
+        }else if(hour>5 && hour< 18){
+            Log.i("PickAMeal","Fetar Time");
+        }else if(hour>18 && hour<24){
+            Log.i("PickAMeal","7elw Time");
+        }
+
+        mHomeActivity.setMeal(quickMeal);
+    }
 }
